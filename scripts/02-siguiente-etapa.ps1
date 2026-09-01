0ff314ef69b4123eb6122c1a327996bccc3933d4
Ángel Velázquez <angelvelazquez98@hotmail.com>
2026-09-01 13:36:41 -0600

    feat: limit actions

--- scripts/02-siguiente-etapa.ps1
+++
@@ -1,4 +0,0 @@
-param([string]$TargetPath = '')
-$runner = Join-Path $PSScriptRoot 'challenge-runner.ps1'
-& $runner -Action next -TargetPath $TargetPath
-
--- scripts/03-estado.ps1
+++
@@ -1,4 +0,0 @@
-param([string]$TargetPath = '')
-$runner = Join-Path $PSScriptRoot 'challenge-runner.ps1'
-& $runner -Action status -TargetPath $TargetPath
-
